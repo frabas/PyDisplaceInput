7f9b98045105425bb9b392ebe97ec4b9740e92a9
Ettore Forigo <ettore.forigo@gmail.com>
2019-05-24 16:49:25 +0200

    Fixed parameters names in init m and init weight importers

--- displace/popsspe/init_m.py
+++ displace/popsspe/init_m.py
@@ -4,5 +4,5 @@ class InitM(PopulationParametersWithSizeGroupImporter):
     def __init__(self):
         super(InitM, self).__init__(
             "popsspe_{name}/init_M_per_szgroup_biolsce{biosce}.dat",
-            "init_M_per_szgroup"
+            "init_M"
         )
--- displace/popsspe/init_weight.py
+++ displace/popsspe/init_weight.py
@@ -4,5 +4,5 @@ class InitWeight(PopulationParametersWithSizeGroupImporter):
     def __init__(self):
         super(InitWeight, self).__init__(
             "popsspe_{name}/init_weight_per_szgroup_biolsce{biosce}.dat",
-            "init_weight_per_szgroup"
+            "init_weight"
         )
