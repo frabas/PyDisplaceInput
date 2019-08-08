bb32bc94327a5cca24bb1599fe590570bff15122
Federico Fuga <fuga@studiofuga.com>
2019-08-08 18:04:54 +0200

    Fix incorrect parameter for nodes/edges loading
    Signed-off-by: Federico Fuga <fuga@studiofuga.com>

--- displace/graphsspe/coordnodes.py
+++ displace/graphsspe/coordnodes.py
@@ -4,12 +4,12 @@ from displace.importer import NSplitsFileImporter
 
 class CoordNodes(NSplitsFileImporter):
     def __init__(self):
-        super().__init__("graphsspe/coord{biosce}.dat", 3, Database.create_nodes)
+        super().__init__("graphsspe/coord{graphsce}.dat", 3, Database.create_nodes)
 
 
 class NodesCodeArea(NSplitsFileImporter):
     def __init__(self):
-        super().__init__("graphsspe/code_area_for_graph{biosce}_points.dat", 3, self._import)
+        super().__init__("graphsspe/code_area_for_graph{graphsce}_points.dat", 3, self._import)
 
     def _import(self, db, linesIt):
         lines = [x[2] for x in linesIt]
--- displace/graphsspe/graphedges.py
+++ displace/graphsspe/graphedges.py
@@ -4,4 +4,4 @@ from displace.importer import NSplitsFileImporter
 
 class GraphEdges(NSplitsFileImporter):
     def __init__(self):
-        super(GraphEdges, self).__init__("graphsspe/graph{biosce}.dat", 3, Database.create_edges)
+        super(GraphEdges, self).__init__("graphsspe/graph{graphsce}.dat", 3, Database.create_edges)
